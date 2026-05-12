91a173b62356b5498b61a642506f62ff85c0410b
Johana Quispe Ruiz <carolinaquispe2740@gmail.com>
2026-05-12 18:51:20 -0500

    Ejercicio4

---
+++ Ejercicio4/src/ejercicio4/ClaseInteres.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio4;
+
+public class ClaseInteres {
+    
+    private static double tasaInteres = 5.0;
+
+    
+    public static void setTasaInteres(double nuevaTasa) {
+        tasaInteres = nuevaTasa;
+    }
+
+
+    public static double getTasaInteres() {
+        return tasaInteres;
+    }
+            
+}
